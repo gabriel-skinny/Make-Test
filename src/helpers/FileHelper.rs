22210bcf14edc4652fb347a5c4c5b323e6ba1003
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-04-30 20:01:01 -0300

    Handling multiple files founded on search

--- src/helpers/FileHelper.rs
+++ src/helpers/FileHelper.rs
@@ -3,6 +3,7 @@ pub mod FileHelper {
     use std::io::{BufReader, Error, Read};
     use std::process::Command;
     use std::str;
+    use dialoguer::MultiSelect;
 
     pub fn read_file(file_path: &str) -> Result<String, Error>{
         let file = File::open(file_path.trim())?;
@@ -15,14 +16,46 @@ pub mod FileHelper {
         Ok(contents)
     }
 
-    pub fn find_file(file_name: &str) -> String {
-       let find_file = Command::new("/bin/find")
+    pub fn find_file(file_name: &str) -> Result<String, Error> {
+       let founded_files = Command::new("/bin/find")
+                                .arg("src/") 
+                                .arg("-type")
+                                .arg("f")
                                 .arg("-name")
-                                .arg(file_name)
-                                .output()
-                                .expect("Could found file"); 
+                                .arg(format!("{}*", file_name))
+                                .output()?;
+    
+       let founded_files_string = str::from_utf8(&founded_files.stdout).unwrap().to_string();
 
-       str::from_utf8(&find_file.stdout).unwrap().to_string()
+    
+
+       println!("File {:?}", founded_files_string);
+       let files = handle_multiple_files(founded_files_string);
+       println!("Files: {:?}", files);
+       let file_chosed = chose_one_file(files);
+       Ok(file_chosed)
     }
 
+    fn handle_multiple_files(files: String) -> Vec<String>{
+       let mut files_result = Vec::new();
+       let mut file_to_push = String::new();
+       for word in files.chars() {
+            if word != '\n' {
+               file_to_push.push(word); 
+            } else {
+              files_result.push(file_to_push.clone());
+              file_to_push.clear();
+            }
+       }
+
+       files_result
+    }
+
+
+    pub fn chose_one_file(file_options: Vec<String>) -> String {
+       let chosen = MultiSelect::new().items(&file_options).interact().unwrap();
+
+       println!("Chosen: {}", file_options[chosen[0]]);
+       file_options[chosen[0]].clone()
+    }
 }
