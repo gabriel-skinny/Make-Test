a7a4492e46dc0996654778e0b7eeb44873c0879f
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-02 22:01:51 -0300

    Refactoring and handling better errors

--- src/helpers/FileHelper.rs
+++ src/helpers/FileHelper.rs
@@ -1,68 +1,79 @@
-#[allow(non_snake_case)]
-pub mod FileHelper {
-    use std::fs::File;
-    use std::io::{BufReader, Error, ErrorKind, Read};
-    use std::process::Command;
-    use std::str;
-    use dialoguer::MultiSelect;
+use std::fs::File;
+use std::io::{BufReader, Error, ErrorKind, Read};
+use std::process::Command;
+use std::str;
+use dialoguer::MultiSelect;
 
-    pub fn read_file(file_path: &str) -> Result<String, Error>{
-        let file = File::open(file_path.trim())?;
-        
-        let mut buffer_read = BufReader::new(file); 
-        let mut contents = String::new();
 
-        buffer_read.read_to_string(&mut contents)?;
-        
-        Ok(contents)
-    }
+pub fn get_content(file_name: &str) -> Result<String, Error> {
+    let file_chosed = find_file(file_name)?;    
 
-    pub fn find_file(file_name: &str) -> Result<String, Error> {
-       let founded_files = Command::new("/bin/find")
-                                .arg("src/") 
-                                .arg("-type")
-                                .arg("f")
-                                .arg("-name")
-                                .arg(format!("{}*", file_name))
-                                .output()?;
+    println!("File chosed {}", file_chosed);
     
-       let founded_files_string = str::from_utf8(&founded_files.stdout)
-                                                .unwrap()
-                                                .to_string();
+    Ok(read_file(&file_chosed)?)
+}
 
-       if !founded_files_string.trim().contains('\n') {
-           return Ok(founded_files_string);
-       }
+fn read_file(file_path: &str) -> Result<String, Error>{
+    let file = File::open(file_path.trim())?;
+    
+    let mut buffer_read = BufReader::new(file); 
+    let mut contents = String::new();
 
-       let files = handle_multiple_files(founded_files_string);
-       let file_chosed = chose_one_file(files)?;
+    buffer_read.read_to_string(&mut contents)?;
+    
+    Ok(contents)
+}
 
-       Ok(file_chosed)
-    }
+fn find_file(file_name: &str) -> Result<String, Error> {
+   let founded_files = Command::new("/bin/find")
+                            .arg("src/") 
+                            .arg("-type")
+                            .arg("f")
+                            .arg("-name")
+                            .arg(format!("{}*", file_name))
+                            .output()?;
 
-    fn handle_multiple_files(files: String) -> Vec<String>{
-       let mut files_result = Vec::new();
-       let mut file_to_push = String::new();
-       for word in files.chars() {
-            if word != '\n' {
-               file_to_push.push(word); 
-            } else {
-              files_result.push(file_to_push.clone());
-              file_to_push.clear();
-            }
-       }
+   let founded_files_string = str::from_utf8(&founded_files.stdout)
+                                            .unwrap()
+                                            .to_string();
+   println!("File name {}", file_name);
 
-       files_result
-    }
+   if founded_files_string == "" {
+        return Err(Error::new(ErrorKind::Other, format!("File not found. Motive: {}", str::from_utf8(&founded_files.stderr).unwrap())))   
+   }
+
+   if !founded_files_string.trim().contains('\n') {
+       return Ok(founded_files_string);
+   }
+
+   let files = format_multiple_files(founded_files_string);
+   let file_chosed = chose_one_file(files)?;
+
+   Ok(file_chosed)
+}
+
+fn format_multiple_files(files: String) -> Vec<String>{
+   let mut files_result = Vec::new();
+   let mut file_to_push = String::new();
+   for word in files.chars() {
+        if word != '\n' {
+           file_to_push.push(word); 
+        } else {
+          files_result.push(file_to_push.clone());
+          file_to_push.clear();
+        }
+   }
+
+   files_result
+}
 
 
-    fn chose_one_file(file_options: Vec<String>) -> Result<String, Error> {
-       let chosen = MultiSelect::new().items(&file_options).interact()?;
-      
-       if chosen.len() < 1 {
-        return Err(Error::new(ErrorKind::Other, "Should select a file"))   
-       }
+fn chose_one_file(file_options: Vec<String>) -> Result<String, Error> {
+   let chosen = MultiSelect::new().items(&file_options).interact()?;
+  
+   if chosen.len() < 1 {
+    return Err(Error::new(ErrorKind::Other, "Should select a file"))   
+   }
 
-       return Ok(file_options[chosen[0]].clone());
-    }
+   return Ok(file_options[chosen[0]].clone());
 }
