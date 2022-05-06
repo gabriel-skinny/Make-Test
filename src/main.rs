0426f5e20570010f8fe17a185ea28db1f1936100
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-06 12:00:25 -0300

    Calling new helper function and handling Writer return

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ fn main() -> Result<(), Error> {
     let file_content = helpers::FileHelper::get_content(&file_name)?;
     let vars = core::Parser::parse_constructor(&file_content)?;
 
-    core::Writer::write_test_file(&vars);
-    
+    let formated_vars_to_write = core::Writer::write_test_file(&vars)?;
+
+    helpers::FileHelper::write_on_file(formated_vars_to_write, "src/results/")?;
+
     println!("Var name: {:?}", vars);
 
     println!("File: \n {}", file_content);
