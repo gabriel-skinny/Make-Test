5d3938be6a1a1cb99ccfdc63611f99df0919543f
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-10 20:55:59 -0300

    Creating function to format path

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
-use std::io::Error;
+use std::io::{Error, ErrorKind};
+use std::path::Path;
 
 mod helpers;
 mod core;
 
+
+fn remove_file_name_from_path(path: &str) -> Result<String, Error> {
+   let mut file_name_end = 0;
+    
+   for index in (0..path.len()).rev() {
+        if path.as_bytes()[index] as char == '/' {
+            file_name_end = index; 
+            break;
+        }
+
+   }  
+
+   if file_name_end == 0 {
+       return Err(Error::new(ErrorKind::Other,"Cannot format path"));
+   }
+
+   Ok(path[0..file_name_end].to_string())
+}
+
 fn main() -> Result<(), Error> {
     let file_name = helpers::EnvHelper::handling_arguments()?;
-    let file_content = helpers::FileHelper::get_content(&file_name)?;
-    let vars = core::Parser::parse_constructor(&file_content)?;
+    let (file_content, file_path) = helpers::FileHelper::get_content(&file_name)?;
 
+    let vars = core::Parser::parse_constructor(&file_content)?;
     let formated_vars_to_write = core::Writer::write_test_file(&vars)?;
 
-    helpers::FileHelper::write_on_file(formated_vars_to_write, "src/results/")?;
-
-    println!("Var name: {:?}", vars);
+    let file_path_formated = remove_file_name_from_path(&file_path)?;
+    helpers::FileHelper::write_on_file(formated_vars_to_write, &file_path_formated)?;
 
     println!("File: \n {}", file_content);
+    println!("Var name: {:?}", vars);
 
     Ok(())
 }
