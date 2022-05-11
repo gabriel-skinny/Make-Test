020bc4784aed0d65b3007b203e4d6f5f39ba7032
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-10 23:07:03 -0300

    Calling nem function

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,10 @@ fn main() -> Result<(), Error> {
     let file_name = helpers::EnvHelper::handling_arguments()?;
     let (file_content, file_path) = helpers::FileHelper::get_content(&file_name)?;
 
-    let vars = core::Parser::parse_constructor(&file_content)?;
+    let mut vars = core::Parser::parse_constructor(&file_content)?;
+    
+    core::Parser::get_imports_for_vars(&file_content, &mut vars, &file_path);
+
     let formated_vars_to_write = core::Writer::write_test_file(&vars)?;
 
     let file_path_formated = remove_file_name_from_path(&file_path)?;
