f7426ef3ab669db1431ced5c8264ea12acaa13a8
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-10 21:28:21 -0300

    Refactoring write on file and functions calls

--- src/helpers/FileHelper.rs
+++ src/helpers/FileHelper.rs
@@ -4,6 +4,7 @@ use std::process::Command;
 use std::str;
 use dialoguer::MultiSelect;
 use std::fs;
+use std::io::Write;
 
 pub fn get_content(file_name: &str) -> Result<(String, String), Error> {
     let file_path = find_file(file_name)?;    
@@ -13,90 +14,85 @@ pub fn get_content(file_name: &str) -> Result<(String, String), Error> {
 
 
 fn find_file(file_name: &str) -> Result<String, Error> {
-   let founded_files = Command::new("/bin/find")
-                            .arg("src/") 
-                            .arg("-type")
-                            .arg("f")
-                            .arg("-name")
-                            .arg(format!("{}*", file_name))
-                            .output()?;
+    let founded_files = Command::new("/bin/find")
+        .arg("src/") 
+        .arg("-type")
+        .arg("f")
+        .arg("-name")
+        .arg(format!("{}*", file_name))
+        .output()?;
 
-   let founded_files_string = str::from_utf8(&founded_files.stdout)
-                                            .unwrap()
-                                            .to_string();
+    let founded_files_string = str::from_utf8(&founded_files.stdout)
+        .unwrap()
+        .to_string();
 
-   if founded_files_string == "" {
+    if founded_files_string == "" {
         return Err(Error::new(ErrorKind::Other, format!("File not found. Motive: {}", str::from_utf8(&founded_files.stderr).unwrap())))   
-   }
+    }
 
-   if !founded_files_string.trim().contains('\n') {
-       return Ok(founded_files_string);
-   }
+    if !founded_files_string.trim().contains('\n') {
+        return Ok(founded_files_string);
+    }
 
-   let files = format_multiple_files(founded_files_string);
-   chose_one_file(files)
+    let files = format_multiple_files(founded_files_string);
+    chose_one_file(files)
+}
+
+fn format_multiple_files(files: String) -> Vec<String>{
+    let mut files_result = Vec::new();
+    let mut file_to_push = String::new();
+    for word in files.chars() {
+        if word != '\n' {
+            file_to_push.push(word); 
+        } else {
+            files_result.push(file_to_push.clone());
+            file_to_push.clear();
+        }
+    }
+
+    files_result
+}
+
+fn chose_one_file(file_options: Vec<String>) -> Result<String, Error> {
+    let chosen = MultiSelect::new().items(&file_options).interact()?;
+
+    if chosen.len() < 1 {
+        return Err(Error::new(ErrorKind::Other, "Should select a file"))   
+    }
+
+    return Ok(file_options[chosen[0]].clone());
 }
 
 fn read_file(file_path: &str) -> Result<String, Error>{
     let file = File::open(file_path.trim())?;
-    
+
     let mut buffer_read = BufReader::new(file); 
     let mut contents = String::new();
 
     buffer_read.read_to_string(&mut contents)?;
-    
+
     Ok(contents)
 }
 
-fn format_multiple_files(files: String) -> Vec<String>{
-   let mut files_result = Vec::new();
-   let mut file_to_push = String::new();
-   for word in files.chars() {
-        if word != '\n' {
-           file_to_push.push(word); 
-        } else {
-          files_result.push(file_to_push.clone());
-          file_to_push.clear();
-        }
-   }
-
-   files_result
-}
-
 pub fn write_on_file(mut content: String, path: &str) -> Result<(), Error> {
-   let to_write_path = format!("{}/tests/test.spec.ts", path);
+    let to_write_path = format!("{}/tests/test.spec.ts", path);
+    let test_dir = format!("{}/tests", path);
 
-   match File::create(to_write_path) {
-    Ok(mut test_file) => {
-       test_file.read_to_string(&mut content)?;    
-    }
-    Err(error) => match error.kind() {
-        ErrorKind::NotFound => match fs::create_dir(format!("{}/tests", path)) {
-           Ok(_dir) => {
-            println!("Creating test directory...");
-            let mut to_write = File::create(format!("{}/tests/test.spec.ts", path))?; 
-            to_write.read_to_string(&mut content)?;    
-           }, 
-           Err(err) => panic!("Erro when creating test directory {:?}", err)
+    if let Err(error) = fs::read_dir(&test_dir) {
+        match error.kind() {
+            ErrorKind::NotFound => {
+                fs::create_dir(test_dir)?; 
+                println!("Creating test directory...");
+            }
+            other_error => {
+                return Err(Error::new(ErrorKind::Other, format!("Error finding test_dir {:?}", other_error)));   
+            }
         }
-        other_error => {
-            panic!("Error when creating file {:?}", other_error) 
-        }
-
     }
-   }
 
 
-    
-   Ok(())
-}
+    let mut file_to_write = File::create(&to_write_path)?; 
+    file_to_write.write_all(&content.as_bytes())?;    
 
-fn chose_one_file(file_options: Vec<String>) -> Result<String, Error> {
-   let chosen = MultiSelect::new().items(&file_options).interact()?;
-  
-   if chosen.len() < 1 {
-    return Err(Error::new(ErrorKind::Other, "Should select a file"))   
-   }
-
-   return Ok(file_options[chosen[0]].clone());
+    Ok(())
 }
