60b91497597ea60d671fbd7fbb4e3c9289022f61
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-10 23:06:54 -0300

    Formating

--- src/core/Writer.rs
+++ src/core/Writer.rs
@@ -11,32 +11,33 @@ pub fn write_test_file(vars: &Vec<Parser::Var>) -> Result<String, Error> {
     Ok(injections)
 }
 
-fn inject_dependencies_on_sut(vars: &Vec<Parser::Var>) -> Result<String, Error> {
-   let mut sut_injection:Option<String> = None;  
-   let mut dependencies_format = String::new();
 
-   for var in vars {
+fn inject_dependencies_on_sut(vars: &Vec<Parser::Var>) -> Result<String, Error> {
+    let mut sut_injection:Option<String> = None;  
+    let mut dependencies_format = String::new();
+
+    for var in vars {
         if !var.is_sut {
             dependencies_format.push_str(&format!("\t{},\n", var.instanciated_name));
         } else {
             sut_injection = Some(format!("{} = new {}", var.instanciated_name, var.class_name)); 
         }
 
-   }
-   
-   if let Some(sut_to_inject) = sut_injection {
-       let injected_dependencies = format!("{}(\n{}\t)", sut_to_inject, dependencies_format);
+    }
 
-       return Ok(injected_dependencies); 
-   }
+    if let Some(sut_to_inject) = sut_injection {
+        let injected_dependencies = format!("{}(\n{}\t)", sut_to_inject, dependencies_format);
 
-   Err(Error::new(ErrorKind::Other, "Sut not found"))   
+        return Ok(injected_dependencies); 
+    }
+
+    Err(Error::new(ErrorKind::Other, "Sut not found"))   
 }
 
 fn making_assignments(vars: &Vec<Parser::Var>) -> Vec<String> {
-   let mut all_assignments = Vec::new();
-   for var in vars {
-       let assignment;  
+    let mut all_assignments = Vec::new();
+    for var in vars {
+        let assignment;  
         if var.is_sut {
             assignment = format!("sut = new {}()", var.class_name);
         } else {
@@ -44,9 +45,9 @@ fn making_assignments(vars: &Vec<Parser::Var>) -> Vec<String> {
         }
 
         all_assignments.push(assignment); 
-   }
+    }
 
 
-   all_assignments 
+    all_assignments 
 }
 
