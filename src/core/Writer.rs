cf9a8c68022e3fe3bd44828e4d989eec7d5a973f
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-11 23:19:09 -0300

    Creating function to type vars and refactoring functions

--- src/core/Writer.rs
+++ src/core/Writer.rs
@@ -4,11 +4,12 @@ use crate::core::Parser;
 pub fn write_test_file(vars: &Vec<Parser::Var>) -> Result<String, Error> {
     let injections = inject_dependencies_on_sut(vars)?; 
     let assignments = making_assignments(vars);
+    let typed_vars = typing_vars(vars);
 
     println!("\n\nSut injection: \n {}\n\n", injections);
     println!("\n\nAssignemnets : \n {}\n\n", assignments);
 
-    Ok(make_test_suit(assignments, injections))
+    Ok(make_test_suit(typed_vars, assignments, injections))
 }
 
 
@@ -37,21 +38,32 @@ fn inject_dependencies_on_sut(vars: &Vec<Parser::Var>) -> Result<String, Error> 
 fn making_assignments(vars: &Vec<Parser::Var>) -> String {
     let mut all_assignments = String::new();
     for var in vars {
-        let assignment;  
         if var.is_sut {
-            assignment = format!("sut = new {}()\n", var.class_name);
+            all_assignments.push_str(&format!("sut = new {}()\n", var.class_name));
         } else {
-            assignment = format!("{} = new {}()\n", var.instanciated_name, var.class_name); 
+            all_assignments.push_str(&format!("{} = new {}()\n", var.instanciated_name, var.class_name)); 
         }
-
-        all_assignments.push_str(&assignment); 
     }
 
 
     all_assignments 
 }
 
-fn make_test_suit(assignments: String, injections: String) -> String {
+fn typing_vars(vars: &Vec<Parser::Var>) -> String {
+    let mut all_typing = String::new();
+    for var in vars {
+        if var.is_sut {
+            all_typing.push_str(&format!("sut: {}\n", var.interface));
+        } else {
+            all_typing.push_str(&format!("{}: {}\n", var.instanciated_name, var.interface)); 
+        }
+    }
+
+
+    all_typing 
+}
+
+fn make_test_suit(typed_vars: String, assignments: String, injections: String) -> String {
    format!("describe('sut_name'), () => {{
     {}
     beforeEach(() => {{)
@@ -59,7 +71,7 @@ fn make_test_suit(assignments: String, injections: String) -> String {
 
         {}
      }})   
-   }}", assignments, assignments, injections)
+   }}", typed_vars, assignments, injections)
 }
 
 
