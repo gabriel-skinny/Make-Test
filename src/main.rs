bc20e3c738f4ca6f833f148f49868b595bc7db2d
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-17 23:32:19 -0300

    Removing util functions from main and calling new modules

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,6 @@ use std::io::{Error, ErrorKind};
 mod helpers;
 mod core;
 
-
-fn remove_file_name_from_path(path: &str) -> Result<String, Error> {
-   let mut file_name_end = 0;
-    
-   for index in (0..path.len()).rev() {
-        if path.as_bytes()[index] as char == '/' {
-            file_name_end = index; 
-            break;
-        }
-
-   }  
-
-   if file_name_end == 0 {
-       return Err(Error::new(ErrorKind::Other,"Cannot format path"));
-   }
-
-   Ok(path[0..file_name_end].to_string())
-}
-
 fn main() -> Result<(), Error> {
     let file_arguments = helpers::EnvHelper::get_arguments()?;
     let (file_content, file_path) = helpers::FileHelper::get_content(&file_arguments)?;
@@ -30,9 +11,11 @@ fn main() -> Result<(), Error> {
     
     core::Parser::get_imports_for_vars(&file_content, &mut vars, &file_path);
 
+    core::MakeMock::make(&file_content);
+
     let formated_vars_to_write = core::Writer::write_test_file(&vars)?;
 
-    let file_path_formated = remove_file_name_from_path(&file_path)?;
+    let file_path_formated = helpers::Utils::remove_file_name_from_path(&file_path)?;
     helpers::FileHelper::write_on_file(formated_vars_to_write, &file_path_formated)?;
 
 
