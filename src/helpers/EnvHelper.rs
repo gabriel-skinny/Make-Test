e4b0bf44ec3452845682673046430727da6682d6
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-28 18:54:05 -0300

    Refactoring envHelper

--- src/helpers/EnvHelper.rs
+++ src/helpers/EnvHelper.rs
@@ -1,5 +1,6 @@
 use std::io::{Error, ErrorKind};
 use std::env;
+use std::collections::HashMap;
 
 #[derive(Default, Debug)]
 pub struct Arguments {
@@ -13,29 +14,53 @@ pub fn get_arguments() -> Result<Arguments, Error>{
         return Err(Error::new(ErrorKind::Other,"too little arguments"));
     }
 
-    let mut arguments = Arguments { filter_path: "src/".to_owned(), ..Default::default()};
+    let mut arguments_hash_map: HashMap<String, String> = make_hash_map();
 
     let argv: Vec<String> = env::args().collect();
     let mut argument_key = 1;
-    for index in 1..argv.len() {
-            let argument_value = argument_key + 1;
-            if argument_value  > argv.len() - 1 || argument_key > argv.len() - 1 { break };
-            handle_argument(&argv[argument_key], &argv[argument_value], &mut arguments)?;
-            argument_key += 2;
+    let mut argument_value = 2;
+    let mut iteration = 0;
+    while argument_value <= argv.len() - 1 || argument_key < argv.len() - 1 {
+        handle_argument(&argv[argument_key], &argv[argument_value], &mut arguments_hash_map)?;
+        argument_key += 2;
+        argument_value = argument_key + 1;
+        iteration += 1;
     }
-    
+
+    let arguments = transform_hash_in_struct(arguments_hash_map);
+
+    if iteration == 0 {
+        return Err(Error::new(ErrorKind::Other, "Unkown command"));
+    }
+
     Ok(arguments)
 }
 
-fn handle_argument(arg: &str, next_argv: &str, arguments: &mut Arguments) -> Result<(), Error> {
-    if arg == "--file" {
-        arguments.file_name = next_argv.to_owned();
-    }
-    else if arg ==  "--filter-path" {
-        arguments.filter_path = next_argv.to_owned();
-    } else {
-        return Err(Error::new(ErrorKind::Other,"Unkown command"));
+fn make_hash_map() -> HashMap<String, String> {
+    HashMap::from([
+                  ("--file".to_owned(), String::new()),
+                  ("--filter-path".to_owned(), String::from("src/")),
+    ])
+}
+
+fn handle_argument(arg: &str, value: &str, arguments: &mut HashMap<String, String>) -> Result<(), Error> {
+    match arguments.get_mut(arg) {
+        Some(key) => {
+            *key = value.to_owned();
+        }
+        None => {
+            return Err(Error::new(ErrorKind::Other,"Unkown command"));
+        }
     }
 
     Ok(())
 }
+
+fn transform_hash_in_struct(hash: HashMap<String, String>) -> Arguments {
+    Arguments {
+        file_name: hash.get("--file").unwrap().to_owned(),
+        filter_path: hash.get("--filter-path").unwrap().to_owned()
+    }
+}
+
+
