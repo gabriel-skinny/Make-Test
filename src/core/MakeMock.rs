e2370f8f522bdced053b4e146a084dfd067436bc
gabriel-skinny <gabrielcatoni12@gmail.com>
2022-05-29 20:32:45 -0300

    Reading imports and making default mock basead on arguments interface

--- src/core/MakeMock.rs
+++ src/core/MakeMock.rs
@@ -2,13 +2,35 @@ use std::io::{Error, ErrorKind};
 
 use crate::helpers::Utils;
 
-pub fn make(content: &str) {
-   let class_lines = get_class_lines(content).unwrap(); 
-   let functions = get_functions(&class_lines).unwrap();
-   let arguments = get_arguments(&functions).unwrap();
-   println!("Lines: \n {:?}", class_lines);
-   println!("Functions: \n {:?}", functions);
-   println!("Arguments: \n {:?}", arguments);
+#[derive(Debug)]
+pub struct Argument { 
+   argument_name: String,
+   interface: String,
+   mock: String,
+}
+
+enum JavaScripTypes {
+    Date,
+    Number,
+    NumberArr,
+    Str,
+    StrArr,
+    Any,
+}
+
+
+pub fn make(content: &str) -> Result<Vec<Argument>, Error> {
+    let class_lines = get_class_lines(content)?; 
+    let functions = get_functions(&class_lines)?;
+    let mut arguments = get_arguments(&functions)?;
+
+    create_mock(&mut arguments);
+    get_imports_for_arguments(&content ,&mut arguments);
+
+    println!("Functions: \n {:?}", functions);
+    println!("Arguments: \n {:?}", arguments);
+
+    Ok(arguments)
 }
 
 fn get_class_lines(content: &str) -> Result<Vec<String>, Error> {
@@ -37,7 +59,7 @@ fn get_class_lines(content: &str) -> Result<Vec<String>, Error> {
 
     }
 
-   Ok(lines) 
+    Ok(lines) 
 }
 
 fn get_functions(class_lines: &Vec<String>) -> Result<Vec<String>, Error> {
@@ -68,34 +90,52 @@ fn get_functions(class_lines: &Vec<String>) -> Result<Vec<String>, Error> {
     if functions.len() < 1 { 
         return Err(Error::new(ErrorKind::Other, "No functions found")); 
     }
-        
+
     Ok(functions)
 }
 
-fn get_arguments(functions: &Vec<String>) -> Result<Vec<String>, Error>{
-    let mut argument = String::new();
+fn get_arguments(functions: &Vec<String>) -> Result<Vec<Argument>, Error>{
+    let mut argument_name = String::new();
     let mut arguments = Vec::new();
-
+    let mut interface = String::new();
 
     for function in functions {
         if function.contains("()") {
             continue;
         } 
 
-        let mut start = false;
+        let mut read_argument_name = false;
+        let mut read_interface = false;
         for word in function.chars() {
             if word == ')' {
-                start = false;
-                arguments.push(argument.clone());
-                argument.clear();
+                read_argument_name = false;
+                read_interface = false;
+                arguments.push(
+                    Argument {
+                        argument_name: argument_name.clone(),
+                        interface: interface.clone(),
+                        mock: String::new()
+                    }
+                    );
+                argument_name.clear();
+                interface.clear();
+            }
+            if read_interface && word != ' ' {
+                interface.push(word);
             }
 
-            if start {
-                argument.push(word);
+            if word == ':' {
+                read_interface = true;
+                read_argument_name = false;
             }
+
+            if read_argument_name {
+                argument_name.push(word);
+            }
+
 
             if word == '(' {
-                start = true;
+                read_argument_name = true;
             }
         }
 
@@ -103,4 +143,65 @@ fn get_arguments(functions: &Vec<String>) -> Result<Vec<String>, Error>{
     }
 
     Ok(arguments)
+}
+
+fn create_mock(arguments: &mut Vec<Argument>)  {
+   for argument in arguments.iter_mut() {
+    if let Some(argument_type) = enumerate_arguments(argument) {
+      match argument_type {  
+        JavaScripTypes::Number => argument.mock = "123".to_owned(),
+        JavaScripTypes::NumberArr => argument.mock = "[1242, 1923]".to_owned(),
+        JavaScripTypes::StrArr => argument.mock = "['teste1', 'teste2']".to_owned(),
+        JavaScripTypes::Str => argument.mock = "teste".to_owned(),
+        JavaScripTypes::Date => argument.mock = "2022-04-03T03:00:00".to_owned(),
+        JavaScripTypes::Any => argument.mock = "any".to_owned(),
+      }
+    };
+   } 
+}
+
+fn enumerate_arguments(argument: &Argument) -> Option<JavaScripTypes> {
+
+   match &argument.interface[..] {
+    "number" => Some(JavaScripTypes::Number),
+    "number[]" => Some(JavaScripTypes::NumberArr),
+    "string[]" => Some(JavaScripTypes::StrArr),
+    "string" => Some(JavaScripTypes::Str),
+    "Date" => Some(JavaScripTypes::Date),
+    "any" => Some(JavaScripTypes::Any),
+    _ => None 
+   } 
+}
+
+
+fn get_imports_for_arguments(content: &str, arguments: &mut Vec<Argument>) {
+    let mut content_line: Vec<&str> = content.split("\n").collect();
+
+    for line in content_line.iter_mut() {
+        for argument in arguments.iter_mut() {
+            if line.contains(&argument.interface) && line.contains("import") && argument.mock != " " {
+                let path = get_path_from_import(line); 
+                println!("Path {}", path);
+            }
+        }
+    }
+
+}
+
+
+fn get_path_from_import(import_line: &str) -> String {
+    let mut start = false;
+    let mut path = String::new();
+
+    for word in import_line.chars() {
+        if word == '"' {
+            start = true; 
+        }
+
+        if start {
+            path.push(word);
+        }
+    }    
+
+    path
 }
